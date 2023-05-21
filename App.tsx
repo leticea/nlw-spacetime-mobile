a0b4c8fd64bd2dc23dc462c338e53aefb6cd690f
leticea <nascimento.leticeah@gmail.com>
2023-05-21 11:28:42 -0300

    feat(app.tsx): text styles

--- App.tsx
+++ App.tsx
@@ -29,7 +29,7 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8"
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
       imageStyle={{ position: "absolute", left: "-100%" }}
     >
       <StyledStripes className="absolute left-2" />
@@ -47,10 +47,19 @@ export default function App() {
           </Text>
         </View>
 
-        <TouchableOpacity activeOpacity={0.7} className="rounded-full bg-green-500 px-5 py-2">
-          <Text className="font-alt text-sm uppercase text-black">Cadastrar lembrança</Text>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
         </TouchableOpacity>
       </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
 
       <StatusBar style="light" />
     </ImageBackground>
