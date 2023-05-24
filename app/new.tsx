c5b95a275765ab6475f75115a66ce74b2ae442d2
leticea <nascimento.leticeah@gmail.com>
2023-05-24 15:57:17 -0300

    feat(new.tsx): new page style

--- app/new.tsx
+++ app/new.tsx
@@ -1,13 +1,22 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Switch, TouchableOpacity, View } from "react-native";
 import Icon from "@expo/vector-icons/Feather";
 
 import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
 import { Link } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useState } from "react";
 
 export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets();
+
+  const [isPublic, setIsPublic] = useState(false);
+
   return (
-    <View className="flex-1 px-8">
-      <View className="flex-row items-center justify-between">
+    <View
+      className="flex-1 px-8"
+      style={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
         <NLWLogo />
 
         <Link href="/memories" asChild>
@@ -15,6 +24,12 @@ export default function NewMemory() {
             <Icon name="arrow-left" size={16} color="#fff" />
           </TouchableOpacity>
         </Link>
+      </View>
+
+      <View className="mt-6 space-y-6">
+        <View className="flex-row items-center gap-2">
+          <Switch value={isPublic} onValueChange={setIsPublic} />
+        </View>
       </View>
     </View>
   );
