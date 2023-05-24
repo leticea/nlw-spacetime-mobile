67ed926f7b61e48d07dba3cabd21d439cd9b12b1
leticea <nascimento.leticeah@gmail.com>
2023-05-24 15:33:36 -0300

    feat(_layout.tsx): isUserAuthenticated function

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,17 +11,29 @@ import {
 } from "@expo-google-fonts/roboto";
 
 import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
-import { Slot, SplashScreen, Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import * as SecureStore from "expo-secure-store";
+import { useEffect, useState } from "react";
 
 const StyledStripes = styled(Stripes);
 
 export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    null | boolean
+  >(null);
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   });
+
+  useEffect(() => {
+    SecureStore.getItemAsync("token").then((token) => {
+      setIsUserAuthenticated(!!token);
+    });
+  }, []);
 
   if (!hasLoadedFonts) {
     return <SplashScreen />;
