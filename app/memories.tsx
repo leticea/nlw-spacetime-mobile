a77a24cd1b5fbe3e3919bb552e42ed7bb50c4467
leticea <nascimento.leticeah@gmail.com>
2023-05-26 11:09:25 -0300

    feat(new.tsx): memories page style

--- app/memories.tsx
+++ app/memories.tsx
@@ -1,9 +1,69 @@
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import Icon from "@expo/vector-icons/Feather";
+import * as SecureStore from "expo-secure-store";
 
-export default function Memories() {
+import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
+import { Link, useRouter } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets();
+  const router = useRouter();
+
+  async function signOut() {
+    await SecureStore.deleteItemAsync("token");
+
+    router.push("/");
+  }
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Memories</Text>
-    </View>
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <NLWLogo />
+
+        <View className="flex-row gap-2">
+          <TouchableOpacity
+            onPress={signOut}
+            className="h-10 w-10 items-center justify-center rounded-full bg-red-500"
+          >
+            <Icon name="log-out" size={16} color="#000" />
+          </TouchableOpacity>
+
+          <Link href="/new" asChild>
+            <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+              <Icon name="plus" size={16} color="#000" />
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </View>
+
+      <View className="mt-6 space-y-10">
+        <View className="space-y-4">
+          <Text>26 de maio, 2023</Text>
+          <View className="space-y-4">
+            <Image
+              source={{
+                uri: "http://192.168.0.116:3333/uploads/b66dc0ad-df8d-4a5c-9669-78d285a478dc.jpg",
+              }}
+              className="aspect-video w-full rounded-lg"
+              alt=""
+            />
+            <Text className="font-body text-base leading-relaxed text-gray-100">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet
+              culpa dolorum unde, voluptas minima deserunt atque. Maxime enim
+              soluta repudiandae at vel. Repellendus voluptatem veniam doloribus
+              iusto fugiat, sint corporis?
+            </Text>
+            <Link href="/memories/id">
+              <Text className="font-body text-sm text-gray-200">Ler mais</Text>
+              <Icon name="arrow-right" size={16} color="#9e9ea0" />
+            </Link>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
