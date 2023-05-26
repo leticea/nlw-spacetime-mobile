6ebc7c2c35de4c8249cd5c744d5e459da2305156
leticea <nascimento.leticeah@gmail.com>
2023-05-26 09:52:22 -0300

    feat(new.tsx): inserting imagePicker

--- app/new.tsx
+++ app/new.tsx
@@ -12,11 +12,30 @@ import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
 import { Link } from "expo-router";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets();
 
+  const [content, setContent] = useState("");
   const [isPublic, setIsPublic] = useState(false);
+
+  async function openImagePicker() {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    // if (!result.canceled) {
+    //   setImage(result.assets[0].uri);
+    // }
+  }
+
+  function handleCreateMemory() {
+    console.log(content, isPublic);
+  }
 
   return (
     <ScrollView
@@ -46,7 +65,11 @@ export default function NewMemory() {
           </Text>
         </View>
 
-        <TouchableOpacity className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20">
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={openImagePicker}
+          className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
+        >
           <View className="flex-row items-center gap-2">
             <Icon name="image" color="#fff" />
             <Text className="font-body text-sm text-gray-200">
@@ -57,6 +80,8 @@ export default function NewMemory() {
 
         <TextInput
           multiline
+          value={content}
+          onChangeText={setContent}
           className="p-0 font-body text-lg text-gray-50"
           placeholderTextColor="#56565a"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
@@ -64,6 +89,7 @@ export default function NewMemory() {
 
         <TouchableOpacity
           activeOpacity={0.7}
+          onPress={handleCreateMemory}
           className="items-center self-end rounded-full bg-green-500 px-5 py-2"
         >
           <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
