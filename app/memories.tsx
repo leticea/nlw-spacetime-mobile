37ce7993ab37f2a2a1aeaf01d83ebcb21bba0d80
leticea <nascimento.leticeah@gmail.com>
2023-05-26 11:17:46 -0300

    feat(new.tsx): memories page style

--- app/memories.tsx
+++ app/memories.tsx
@@ -18,10 +18,10 @@ export default function NewMemory() {
 
   return (
     <ScrollView
-      className="flex-1 px-8"
+      className="flex-1"
       contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
     >
-      <View className="mt-4 flex-row items-center justify-between">
+      <View className="mt-4 flex-row items-center justify-between px-8">
         <NLWLogo />
 
         <View className="flex-row gap-2">
@@ -42,8 +42,13 @@ export default function NewMemory() {
 
       <View className="mt-6 space-y-10">
         <View className="space-y-4">
-          <Text>26 de maio, 2023</Text>
-          <View className="space-y-4">
+          <View className="flex-row items-center gap-2">
+            <View className="h-px w-5 bg-gray-50" />
+            <Text className="font-body text-xs text-gray-100">
+              26 de maio, 2023
+            </Text>
+          </View>
+          <View className="space-y-4 px-8">
             <Image
               source={{
                 uri: "http://192.168.0.116:3333/uploads/b66dc0ad-df8d-4a5c-9669-78d285a478dc.jpg",
@@ -57,9 +62,13 @@ export default function NewMemory() {
               soluta repudiandae at vel. Repellendus voluptatem veniam doloribus
               iusto fugiat, sint corporis?
             </Text>
-            <Link href="/memories/id">
-              <Text className="font-body text-sm text-gray-200">Ler mais</Text>
-              <Icon name="arrow-right" size={16} color="#9e9ea0" />
+            <Link href="/memories/id" asChild>
+              <TouchableOpacity className="flex-row items-center gap-2">
+                <Text className="font-body text-sm text-gray-200">
+                  Ler mais
+                </Text>
+                <Icon name="arrow-right" size={16} color="#9e9ea0" />
+              </TouchableOpacity>
             </Link>
           </View>
         </View>
